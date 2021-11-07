0b36e8df077a5abb95f5eabb2b8cedba1ddbfb2e
erkanmikiver <89485772+erkanmikiver@users.noreply.github.com>
2021-11-07 21:30:29 +0000

    Create powershellscript.ps1

---
+++ powershellscript.ps1
@@ -0,0 +1,3 @@
+Function Set-Speaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..$Volume | % {$wshShell.SendKeys([char]175)}}
+Set-Speaker -Volume 50
+Set-Speaker -Volume 50
